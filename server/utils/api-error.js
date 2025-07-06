208198448f5db3b335124a261577741b1462ce39
aryanaryan <103895665+aryankumar-dev@users.noreply.github.com>
2025-07-06 22:22:55 +0530

    created utils for Apierror api response asycn handlerst

--- server/utils/api-error.js
+++ server/utils/api-error.js
@@ -1,13 +1,28 @@
 class ApiError extends Error {
-constructor(  statusCode, message = "Something went wrong", errors = [], stack = "") {
+  constructor(statusCode, message = "Something went wrong", errors = [], stack = "") {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.success = false;
+    this.errors = errors;
+    this.message = message;
 
-  super
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+
+  toJSON() {
+    return {
+      statusCode: this.statusCode,
+      data: this.data,
+      success: this.success,
+      errors: this.errors,
+      message: this.message,
+    };
+  }
 }
 
-
-
-
 export { ApiError };
-
-
-
